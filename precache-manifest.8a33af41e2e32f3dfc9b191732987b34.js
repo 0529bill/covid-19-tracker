c804af935f9f6a352513aa8b7cd092cb861d6656
water <0529bill@gmail.com>
2020-08-19 22:04:24 +0800

    Updates

---
+++ precache-manifest.8a33af41e2e32f3dfc9b191732987b34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e408757d978b0fbfd32e7c59848546",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "34b9b106691f4eaaf1f2",
+    "url": "/covid-19-tracker/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "98a7fdfec34fde94832e",
+    "url": "/covid-19-tracker/static/css/main.c3f6051b.chunk.css"
+  },
+  {
+    "revision": "34b9b106691f4eaaf1f2",
+    "url": "/covid-19-tracker/static/js/2.47ee8178.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/covid-19-tracker/static/js/2.47ee8178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a7fdfec34fde94832e",
+    "url": "/covid-19-tracker/static/js/main.d3f9fc97.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
